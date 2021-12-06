a48edd9b6ffff0e0576ac3b1b9642f33afc39c6d
forkich <meshcher.aln@gmail.com>
2021-12-06 16:20:27 +0300

    add views folder

---
+++ app/src/main/java/com/inrealar/views/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.proct.activities.inreal.views
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.findNavController
+import com.proct.activities.inreal.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainNavController: NavController
+    private lateinit var toolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mainNavController = this.findNavController(R.id.mainNavHostFragment)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationIcon(R.drawable.arrow_back)
+        toolbar.setNavigationOnClickListener {
+            mainNavController.navigateUp()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.mainOrderFragment -> {
+                val navOptions =
+                    NavOptions.Builder().setPopUpTo(R.id.mainOrderFragment, false).build()
+                mainNavController.navigate(R.id.mainOrderFragment, null, navOptions)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
